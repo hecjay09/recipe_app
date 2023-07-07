4fbf51c7470bb111863507fc0b9d544d3a0ed630
Hector Onato <hecxtoronato@gmail.com>
2023-07-07 11:49:20 -0700

    added docker

--- server/postgres.js
+++ server/postgres.js
@@ -2,7 +2,7 @@ const { Pool } = require("pg");
 //not recommended, change this later
 var pool = new Pool({
     user: "postgres",
-    host: "localhost",
+    host: "db",
     port: "5432",
     database: "recipe",
     password: "1643",
